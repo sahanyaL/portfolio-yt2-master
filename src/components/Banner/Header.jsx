cd72ec1eebad8ef2de5ca8c03007f4a1e99b7342
sahanyaL <132870882+sahanyaL@users.noreply.github.com>
2024-04-09 17:36:28 +0530

    update the header.jsx

--- src/components/Banner/Header.jsx
+++ src/components/Banner/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
             <span><a href="#home">Home</a></span>
             <span><a href="#service">Services</a></span>
             <span><a href="#project">Projects</a></span>
-            <span><a href="#client">Testimonials</a></span>
+            <span><a href="#client">Certificates</a></span>
             <span><a href="#footer">Portfolio</a></span>
         </Nav>
         <div
